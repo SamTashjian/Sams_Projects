04838c4d61f3b5b26a8d9f781ba1dd3162d3bf42
Sam <stashjia@kent.edu>
2016-06-05 13:09:03 -0400

    SATAINDWRK-5

---
+++ Warmup.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Warmups;
+namespace Warmup.Tests
+{
+    [TestFixture]
+     
+    public class Class1
+    {
+        [TestCase("Bob", "Hello Bob!")]
+        [TestCase("Alice","Hello Alice!")]
+        [TestCase("X", "Hello X!")]
+        public void TestSayHello(string name, string expected)
+        {
+            Strings obj = new Strings();
+            string testValue = obj.SayHi(name);
+
+            Assert.AreEqual(expected, testValue);
+        }
+    }
+}
